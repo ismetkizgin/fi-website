01a577bcb39181d62ae449e32efcc45ea77eaa3f
ismet kizgin <info@ismetkizgin.com>
2021-01-14 01:07:01 +0300

    fix: fix for task log window

--- src/app/components/log-detail/log-detail.component.ts
+++ src/app/components/log-detail/log-detail.component.ts
@@ -7,22 +7,21 @@ import { TaskLog } from '../../models';
 @Component({
   selector: 'app-log-detail',
   templateUrl: './log-detail.component.html',
-  styleUrls: ['./log-detail.component.scss']
+  styleUrls: ['./log-detail.component.scss'],
 })
 export class LogDetailComponent implements OnInit {
-
   constructor(
     private _activateRoute: ActivatedRoute,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private _taskLogService:TaskLogService
-  ) { }
+    private _taskLogService: TaskLogService
+  ) {}
 
   logControl: boolean = false;
-  ProjectId=this.data.ProjectId;
-  Id=this.data.Id;
-  _model= this.data._model;
-  logs: Array<TaskLog>;
-  searchText="";
+  ProjectId = this.data.ProjectId;
+  Id = this.data.Id;
+  _model = this.data._model;
+  taskLogs: Array<TaskLog>;
+  searchText = '';
   paginationConfig = {
     id: 'logs',
     itemsPerPage: 5,
@@ -30,16 +29,15 @@ export class LogDetailComponent implements OnInit {
   };
 
   async ngOnInit() {
-    if(this.Id!=null){
-      this.logControl=true;
+    if (this.Id != null) {
+      this.logControl = true;
+      try {
+        this.taskLogs = <Array<TaskLog>>(
+          await this._taskLogService.listAsync(this.Id)
+        );
+      } catch (error) {
+        this._taskLogService.errorNotification(error);
+      }
     }
-    try{
-      this.logs= <Array<TaskLog>> await this._taskLogService.listAsync(this.Id);
-    }
-    catch(error){
-      this._taskLogService.errorNotification(error);
-    }
-    console.log(this.logs)
   }
-
 }
