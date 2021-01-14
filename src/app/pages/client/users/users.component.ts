658c1d22f387b390feb4f427a092aba2b687a5c5
ismet kizgin <info@ismetkizgin.com>
2021-01-14 13:53:38 +0300

    refactor: project user list page revised

--- src/app/pages/client/users/users.component.ts
+++ src/app/pages/client/users/users.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models';
 import { ProjectUserService } from '../../../utils';
 import { ActivatedRoute } from '@angular/router';
-import { AddProjectUserComponent, DialogWindowComponent } from 'src/app/components';
+import {
+  AddProjectUserComponent,
+  DialogWindowComponent,
+} from 'src/app/components';
 import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -10,21 +13,20 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
-
   constructor(
     private _projectUserService: ProjectUserService,
     private activatedRoute: ActivatedRoute,
     private _dialog: MatDialog,
     private _snackBar: MatSnackBar,
-    private _translateService: TranslateService,
-  ) { }
+    private _translateService: TranslateService
+  ) {}
 
   users: Array<any>;
-  searchText="";
-  Id:string;
+  searchText = '';
+  Id: string;
   paginationConfig = {
     id: 'users',
     itemsPerPage: 20,
@@ -32,20 +34,24 @@ export class UsersComponent implements OnInit {
   };
 
   async ngOnInit() {
-    const Id= this.activatedRoute.snapshot.paramMap.get('Id');
-    this.Id=Id;
-    try{
-      this.users= <Array<User>> await this._projectUserService.listAsync(Id);
-    }catch(error){
-      this._projectUserService.errorNotification(error)
+    const Id = this.activatedRoute.snapshot.paramMap.get('Id');
+    this.Id = Id;
+    try {
+      this.users = <Array<User>>await this._projectUserService.listAsync(Id);
+    } catch (error) {
+      this._projectUserService.errorNotification(error);
     }
   }
 
   openAddProjectUser() {
-    this._dialog.open(AddProjectUserComponent, {
-      width: "400px",
-      data: this.Id
-    })
+    const diologRef = this._dialog.open(AddProjectUserComponent, {
+      width: '400px',
+      data: this.Id,
+    });
+
+    diologRef.afterClosed().subscribe((result: any) => {
+      if (result) this.ngOnInit();
+    });
   }
 
   async projectUserDelete(Id) {
@@ -81,5 +87,4 @@ export class UsersComponent implements OnInit {
       }
     });
   }
-
 }
